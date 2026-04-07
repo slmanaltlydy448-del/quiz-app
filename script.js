755c56364bfedaaf68a7f24e88556944824e32fa
slmanaltlydy448-del <slmanaltlydy448@gmail.com>
2026-04-07 20:29:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function startApp() {
     document.getElementById("welcome").style.display = "none";
 }
+function openSupport(){
+    document.getElementById("supportBox").style.display = "block";
+}
+
+function closeSupport(){
+    document.getElementById("supportBox").style.display = "none";
+}
